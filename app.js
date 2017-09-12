8896cfe11bc3fb0fd8defbc7ad51c325baf1af16
youjingyu <735284268@qq.com>
2017-09-12 18:08:06 +0800

    server allow cross domain request

--- app.js
+++ app.js
@@ -23,6 +23,14 @@ app.use(bodyParser.urlencoded({extended: false, limit: '50mb'}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.all('*', function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    next();
+});
+
 app.use('/', index);
 app.use('/users', users);
 app.use('/goods', goods);
